10eafcac5e2e0953da807a0935fc5d5c15babbd7
HasalKenula <hasalkenulagmail.com>
2025-02-11 19:17:16 +0530

    added navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Navbar from "./NavBar";
 
-function Home(){
-    const{logout}=useAuth();
-    return(
+function Home() {
+    const { logout } = useAuth();
+    return (
+
         <div>
-                <p>ewereryey</p>
-                <div>
+            <div>
+                <Navbar />
+            </div>
+            <p>ewereryey</p>
+            <div>
                 <Link to="/category" className="bg-gray-800 text-white px-5 py-2 me-3">Category</Link>
                 <Link to="/student" className="bg-gray-800 text-white px-5 py-2 me-3">Student</Link>
-                 {/* <Link to="/item" className="bg-gray-800 text-white px-5 py-2 me-3">Item</Link>
+                {/* <Link to="/item" className="bg-gray-800 text-white px-5 py-2 me-3">Item</Link>
                 <Link to="/stock" className="bg-gray-800 text-white px-5 py-2 me-3">Stock</Link>
                 <Link to="/profile" className="bg-gray-800 text-white px-5 py-2 me-3">Setting</Link> */}
-                <button className="bg-gray-800 text-white px-5 py-2 me-3" onClick={logout}>Logout</button> 
+                <button className="bg-gray-800 text-white px-5 py-2 me-3" onClick={logout}>Logout</button>
             </div>
+
         </div>
     )
 }
