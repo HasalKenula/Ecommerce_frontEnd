964ca59b00afe5566e048a5085a92481a0297377
HasalKenula <hasalkenulagmail.com>
2025-02-14 23:43:20 +0530

    created first section in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import Navbar from "./NavBar";
+import Button from "./Button";
+
+import Auto from "./AutoPlay";
 
 function Home() {
     const { logout } = useAuth();
@@ -10,7 +13,33 @@ function Home() {
             <div>
                 <Navbar />
             </div>
-            <p>ewereryey</p>
+            <div className="w-full lg:h-[700px] bg-green-300 flex flex-col lg:flex-row md-[1500px]">
+                <div className="lg:w-1/2 bg-blue-300 lg:mt-[24px] lg:mb-[24px] lg:ml-[24px] flex  justify-center items-center ">
+                    <div className=" flex  flex-col justify-center items-center   p-[6px]">
+                        <div className="text-6xl font-bold mt-[24px] ">
+                        Get Best Device  <br/>
+                        With Lowest Price
+                        </div>
+
+                        <div className="m-24 0 24 32 ">
+                        Upgrade your tech without breaking the bank! 
+                        Find top-quality devices
+                         at unbeatable prices, ensuring performance,
+                          reliability, and value—all in one place. 
+                        </div>
+
+                        <div>
+                        <Button/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="lg:w-1/2 bg-yellow-300 mt-[24px] lg:mb-[24px] lg:mr-[24px]">
+                      <Auto/>
+                </div>
+
+            </div>
             <div>
                 <Link to="/category" className="bg-gray-800 text-white px-5 py-2 me-3">Category</Link>
                 <Link to="/student" className="bg-gray-800 text-white px-5 py-2 me-3">Student</Link>
