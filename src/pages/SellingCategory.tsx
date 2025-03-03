8ecffb2c412e209e66f96a6d0c9624f051fcbebd
HasalKenula <hasalkenula255@gmail.com>
2025-03-03 20:55:27 +0530

    changed section two in page

--- src/pages/SellingCategory.tsx
+++ src/pages/SellingCategory.tsx
@@ -7,14 +7,14 @@ import AppleWatch2 from '../assets/AppleWatch2.png'
 function SellingCategory() {
     return (
         <div>
-            <div className="w-full lg:h-[800px] bg-yellow-300 ">
+            <div className="w-full lg:h-[800px] bg-yellow-300 md:text-2xl text-lg ">
                 <div className="flex lg:flex-row flex-col lg:h-1/2 lg:mb-24 max-sm:items-center ">
                     <div className='lg:w-2/6 bg-blue-200  '>
                         <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center'>
 
-                            <img src={Laptop2} className='w-2/3 h-2/3 object-contain items-center' />
+                            <img src={Laptop2} className='w-2/3 h-2/3 object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto ' />
 
-                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px] items-center'>Laptops</button>
+                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]  md:p-[24px] items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto'>Laptops</button>
 
 
                         </div>
@@ -22,9 +22,9 @@ function SellingCategory() {
                     <div className='lg:w-2/6 bg-blue-200 '>
                         <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center'>
 
-                            <img src={Headphone2} className='w-2/3 h-2/3 object-contain items-center' />
+                            <img src={Headphone2} className='w-2/3 h-2/3 object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto' />
 
-                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px] items-center'>Headphones</button>
+                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]  md:p-[24px] items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto'>Headphones</button>
 
 
                         </div>
@@ -32,9 +32,9 @@ function SellingCategory() {
                     <div className='lg:w-2/6 bg-blue-200 '>
                         <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center '>
 
-                            <img src={AppleWatch2} className='w-2/3 h-2/3 object-contain items-center' />
+                            <img src={AppleWatch2} className='w-2/3 h-2/3 object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto' />
 
-                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px] items-center'>Apple Watches</button>
+                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]  md:p-[24px] items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto'>Watches</button>
 
 
                         </div>
@@ -49,19 +49,9 @@ function SellingCategory() {
                     <div className='lg:w-2/6 bg-blue-200 '>
                         <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center '>
 
-                            <img src={Playstation} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2 object-contain items-center ' />
+                            <img src={Playstation} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2 object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto' />
 
-                            <button className='w-4/8 rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]  '>Playstation</button>
-
-
-                        </div>
-                    </div>
-                    <div className='lg:w-2/6 bg-blue-200 '>
-                        <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center'>
-
-                            <img src={Tablets2} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2 object-contain items-center' />
-
-                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px] items-center'>Tablets </button>
+                            <button className='w-4/8 rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]   md:p-[24px] lg:mr-0 lg:ml-0 md:mr-8 ml-auto'>Playstation</button>
 
 
                         </div>
@@ -69,9 +59,19 @@ function SellingCategory() {
                     <div className='lg:w-2/6 bg-blue-200 '>
                         <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center'>
 
-                            <img src={Iphone} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2  object-contain items-center ' />
+                            <img src={Tablets2} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2 object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto' />
 
-                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px] items-center'>iPhones</button>
+                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px] p-[8px]  md:p-[24px] items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto'>Tablets </button>
+
+
+                        </div>
+                    </div>
+                    <div className='lg:w-2/6 bg-blue-200 '>
+                        <div className='mx-[8px]  lg:h-full bg-green-300 flex lg:flex-col justify-center items-center '>
+
+                            <img src={Iphone} className='lg:w-2/3 lg:h-2/3  w-1/2 h-1/2  object-contain items-center lg:mr-0 lg:ml-0 md:mr-8 ml-auto' />
+
+                            <button className='w-4/8  rounded-xl bg-purple-400 text-white lg:m-8 lg:p-[12px]  md:p-[24px] p-[8px] items-center md:mr-8 ml-auto'>iPhones</button>
 
 
                         </div>
