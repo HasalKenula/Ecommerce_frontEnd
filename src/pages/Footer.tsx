1eb35fa070d640aebb3e7692970beeb33a5e9fbd
HasalKenula <hasalkenula255@gmail.com>
2025-03-05 21:57:59 +0530

    changed the colour to red in footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <div>
-            <div className="w-full lg:h-[700px] bg-green-300 flex flex-col lg:flex-row md:h-[1320px] h-[850px]">
+            <div className="w-full lg:h-[700px] bg-red-300 flex flex-col lg:flex-row md:h-[1320px] h-[850px]">
                 footer section
             </div>
         </div>
