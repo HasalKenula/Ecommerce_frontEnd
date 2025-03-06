2b7cae8ad6467a2590b8c765d6705561b5ff8646
HasalKenula <hasalkenula255@gmail.com>
2025-03-06 21:23:30 +0530

    added a paragraph

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -2,7 +2,11 @@ function Footer() {
     return (
         <div>
             <div className="w-full lg:h-[700px] bg-red-300 flex flex-col lg:flex-row md:h-[1320px] h-[850px]">
-                footer section
+                 <div>
+                 footer <br />
+                 
+                 A grayscale image with pixel values ranging from 0 to 255 can have a CLUT that maps those grayscale values to a specific set of colors, creating a false-color image.
+                </div>
             </div>
         </div>
     )
