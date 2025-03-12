28f41f1862dfc1805b75a0706d737730ee577138
HasalKenula <hasalkenula255@gmail.com>
2025-03-12 22:50:54 +0530

    added a white colour to footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -6,9 +6,11 @@ function Footer() {
                     footer <br />
 
                     A grayscale image with pixel values ranging from 0 to 255 can have a CLUT that maps those grayscale values to a specific set of colors, creating a false-color image.
-                    <div >
+                    <div className="text-white">
                     A grayscale image with pixel values ranging from 0 to 255 can have
                     </div>
+                    footer
+                    A grayscale image with pixel values ranging from 0 to 255 can have
                     footer
                     A grayscale image with pixel values ranging from 0 to 255 can have
                 </div>
