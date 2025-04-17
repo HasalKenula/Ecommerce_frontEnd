7c01943750324d83611396cf17aac1f83b16f7e2
HasalKenula <hasalkenula255@gmail.com>
2025-04-17 22:35:06 +0530

    changed the footer and add two images

--- src/pages/ProductsAbout.tsx
+++ src/pages/ProductsAbout.tsx
@@ -1,42 +1,45 @@
 import Button from "./Button";
-import Cat from '../assets/Cat.jpg';
+import Tab from '../assets/AppleAir.png';
+import Watch from '../assets/AppleWatch.jpeg';
 function ProductsAbout() {
     return (
         <div>
-            <div className="w-full lg:h-[700px] bg-purple-300 flex flex-col lg:flex-row ">
-                <div className="lg:w-1/2 bg-blue-300 lg:mt-[24px] lg:mb-[24px] lg:ml-[24px]  ">
-                    <div className=" flex  flex-col justify-center items-center   p-[6px]">
-                        <div className="md:text-6xl text-4xl font-bold mt-[24px] ">
-                            Get Best Device  <br />
-                            With Lowest Price
+            <div className="w-full lg:h-[700px]  flex flex-col lg:flex-row text-white ">
+                <div className=" lg:w-1/2 h-[500px] lg:h-auto bg-gray-800 lg:mt-[24px] lg:mb-[24px] lg:ml-[24px] relative ">
+               <img src={Tab} alt="iPad Air" className="absolute inset-0 w-full h-full object-cover opacity-20" /> 
+                     <div className=" absolute flex  flex-col inset-0 justify-center items-center   p-[6px] ">
+                        
+                        <div className=" md:text-6xl text-4xl font-bold mt-[24px] ">
+                        Apple  <br />
+                            iPad Aire
                         </div>
 
-                        <div className="md:m-24 0 24 32 m-12 0 12 8 ">
-                            Upgrade your tech without breaking the bank!
-                            Find top-quality devices
-                            at unbeatable prices, ensuring performance,
-                            reliability, and value—all in one place.
+                        <div className=" md:m-24 0 24 32 m-12 0 12 8 ">
+                        Power meets portability with the iPad Air. Featuring a stunning 
+                        Liquid Retina display, the powerful Apple M1 chip, and support for Apple Pencil 
+                        and Magic Keyboard, it's perfect for work, creativity, and entertainment—all in 
+                        a sleek, lightweight design.
                         </div>
 
                         <div items-center>
                             <Button />
                         </div>
-                    </div>
+                    </div> 
 
                 </div>
 
-                <div className="lg:w-1/2 bg-red-300 lg:mt-[24px] lg:mb-[24px] lg:mr-[24px]">
-                <div className=" flex  flex-col justify-center items-center   p-[6px]">
+                <div className=" lg:w-1/2 h-[500px] lg:h-auto bg-gray-800 lg:mt-[24px] lg:mb-[24px] lg:mr-[24px] relative">
+                <img src={Watch} alt="iPad Air" className="absolute inset-0 w-full h-full object-cover opacity-20" /> 
+                <div className=" absolute flex  inset-0 flex-col justify-center items-center   p-[6px]">
                         <div className="md:text-6xl text-4xl font-bold mt-[24px] ">
-                            Get Best Device  <br />
-                            With Lowest Price
+                           Apple  <br />
+                           Watch
                         </div>
 
                         <div className="md:m-24 0 24 32 m-12 0 12 8 ">
-                            Upgrade your tech without breaking the bank!
-                            Find top-quality devices
-                            at unbeatable prices, ensuring performance,
-                            reliability, and value—all in one place.
+                        Stay connected, fit, and stylish with the SMART Apple Watch. 
+                        Track your workouts, monitor your health, receive notifications,
+                         and enjoy seamless integration with your Apple devices—all from your wrist.
                         </div>
 
                         <div items-center>
