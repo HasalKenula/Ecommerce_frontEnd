5eb2233f2712e85d9466cf5f2b0b5470bf3cdfad
HasalKenula <hasalkenula255@gmail.com>
2025-04-18 13:15:47 +0530

    added colors to deals page

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -3,8 +3,8 @@ import VRMan from "../assets/VRMan.png"
 
 function Deals() {
     return (
-        <div className="w-full lg:h-[700px]  flex flex-col lg:flex-row md:h-[1320px] h-[850px]">
-            <div className="lg:w-1/2  lg:mt-[24px] lg:mb-[24px] lg:ml-[24px] flex  justify-center items-center ">
+        <div className="w-full lg:h-[700px] bg-green-300  flex flex-col lg:flex-row md:h-[1320px] h-[850px]">
+            <div className="lg:w-1/2 bg-blue-300  lg:mt-[24px] lg:mb-[24px] lg:ml-[24px] flex  justify-center items-center ">
                 <div className=" flex  flex-col justify-center items-center   p-[6px]">
                     <div className="md:text-6xl text-4xl font-bold mt-[24px] ">
                     Get Virtual Reality <br />
