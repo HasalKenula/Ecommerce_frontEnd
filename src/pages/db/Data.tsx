47e88fa1738ee55bf6e9081ec59ba07327cbd9a3
HasalKenula <hasalkenula255@gmail.com>
2025-04-27 23:37:21 +0530

    added some data to page

---
+++ src/pages/db/Data.tsx
@@ -0,0 +1,85 @@
+
+import im1 from "../assec/AppleWatch2.png";
+import im2 from "../assec/Headphone2.jpg";
+import im3 from "../assec/Iphone.png";
+import im4 from "../assec/Laptop2.png";
+import im5 from "../assec/PlayStation.png";
+import im6 from "../assec/Tablet2.jpg";
+
+const Data = [
+  {
+    img:im1 ,
+    title: "Nike Air Monarch IV",
+    
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "200",
+    company: "Nike",
+    color: "white",
+    category: "sneakers",
+  },
+  {
+    img: im2,
+    title: "Nike Air Vapormax Plus",
+   
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "200",
+    company: "Nike",
+    color: "red",
+    category: "sneakers",
+  },
+
+  {
+    img:im3,
+    title: "Nike Waffle One Sneaker",
+   
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "200",
+    company: "Nike",
+    color: "green",
+    category: "sneakers",
+  },
+  {
+    img: im4,
+    title: "Nike Running Shoe",
+    
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "200",
+    company: "Adidas",
+    color: "black",
+    category: "sneakers",
+  },
+  {
+    img: im5,
+    title: "Flat Slip On Pumps",
+    
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "200",
+    company: "Vans",
+    color: "green",
+    category: "flats",
+  },
+  {
+    img: im6,
+    title: "Knit Ballet Flat",
+   
+    reviews: "(123 reviews)",
+    prevPrice: "$140,00",
+    newPrice: "50",
+    company: "Adidas",
+    color: "black",
+    category: "flats",
+  },
+
+  
+  
+ 
+
+ 
+];
+
+export default Data;
